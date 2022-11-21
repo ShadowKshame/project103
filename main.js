6cc423f0dd86486eee033cc86410462e29635f65
ShadowKshame <104213448+ShadowKshame@users.noreply.github.com>
2022-11-20 19:31:47 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -21,5 +21,22 @@ classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models
 
 function modelLoaded()
 {
-    console.log("modelLoaded");
+    console.log("modelLoaded!");
+}
+
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("objectname").innerHTML = results[0].label;
+        document.getElementById("objectaccuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
 }
